3c57d5e520b1f008c64998c0059e0afdf984a2c6
Tim Combs <tim00combs@gmail.com>
2019-05-10 23:26:44 -0700

    added assets.js to src/client - it uploads assets to an assets object for easy retrieval, changed eslint.rc because I like to use parens consistently with arrow functions - EVERYTHING gets parens - and changed networking.js to reflect this change.

--- src/client/networking.js
+++ src/client/networking.js
@@ -4,14 +4,14 @@ import { processGameUpdate } from './state';
 const Constants = require('../shared/constants');
 
 const socket = io(`ws://${window.location.host}`);
-const connectedPromise = new Promise(resolve => {
+const connectedPromise = new Promise((resolve) => {
   socket.on('connect', () => {
     console.log('Connected to server!');
     resolve();
   });
 });
 
-export const connect = onGameOver => (
+export const connect = (onGameOver) => (
   connectedPromise.then(() => {
     // Register callbacks
     socket.on(Constants.MSG_TYPES.GAME_UPDATE, processGameUpdate);
@@ -19,10 +19,10 @@ export const connect = onGameOver => (
   })
 );
 
-export const play = username => {
+export const play = (username) => {
   socket.emit(Constants.MSG_TYPES.JOIN_GAME, username);
 };
 
-export const updateDirection = dir => {
+export const updateDirection = (dir) => {
   socket.emit(Constants.MSG_TYPES.INPUT, dir);
 };
