458ce043f2b1655f96b587afd90cc0cd26636c13
Tim Combs <tim00combs@gmail.com>
2019-05-12 15:06:56 -0700

    finished game.js

--- src/server/game.js
+++ src/server/game.js
@@ -49,7 +49,73 @@ class Game {
     this.bullets = this.bullets.filter((bullet) => {
       return !bulletsToRemove.includes(bullet);
     });
+
+    // update each player
+    Object.keys(this.sockets).forEach((playerID) => {
+      const player = this.players[playerID];
+      const newBullet = player.update(dt);
+      if (newBullet) {
+        this.bullets.push(newBullet);
+      }
+    });
+
+    // apply collisions, give players score for hitting bullets
+    const destroyedBullets = applyCollisions(Object.values(this.players), this.bullets);
+    destroyedBullets.forEach((b) => {
+      if (this.players[b.parentID]) {
+        this.players[b.parentID].onDealtDamage();
+      }
+    });
+    this.bullets = this.bullets.filter((bullet) => {
+      return !destroyedBullets.includes(bullet);
+    });
+
+    // check for dead players
+    Object.keys(this.sockets).forEach((playerID) => {
+      const socket = this.sockets[playerID];
+      const player = this.players[playerID];
+
+      if (player.hp <= 0) {
+        socket.emit(Constants.MSG_TYPES.GAME_OVER);
+        this.removePlayer(socket);
+      }
+    });
+
+    // send game update to each player every other time
+    if (this.shouldSendUpdate) {
+      const leaderboard = this.getLeaderboard();
+      Object.keys(this.sockets).forEach((playerID) => {
+        const socket = this.sockets[playerID];
+        const player = this.players[playerID];
+        socket.emit(Constants.MSG_TYPES.GAME_UPDATE, this.createUpdate(player, leaderboard));
+      });
+    }else{
+      this.shouldSendUpdate = true;
+    }
   }
 
+  getLeaderboard() {
+    return Object.values(this.players)
+      .sort((p1, p2) => p2.score - p1.score)
+      .slice(0, 5)
+      .map((p) => ({ username: p.username, score: Math.round(p.score) }));
+  }
 
+  createUpdate(player, leaderboard) {
+    const nearbyPlayers = Object.values(this.players)
+      .filter((p) => p !== player &&
+                     p.distanceTo(player) <= Constants.MAP_SIZE / 2);
+    const nearbyBullets = this.bullets
+      .filter((b) => b.distanceTo(player) <= Constants.MAP_SIZE / 2);
+
+    return {
+      t: Date.now(),
+      me: player.serializeForUpdate(),
+      others: nearbyPlayers.map((p) => p.serializeForUpdate()),
+      bullets: nearbyBullets.map((p) => p.serializeForUpdate()),
+      leaderboard,
+    };
+  }
 }
+
+module.exports = Game;
